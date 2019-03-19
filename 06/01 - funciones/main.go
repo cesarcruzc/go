06bab2dc65110d489f85a7395819108572d8188f
Cesar Cruz <cesarcruz@pop-os.localdomain>
2019-03-19 15:23:45 -0500

    Functions

---
+++ 06/01 - funciones/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello 0")
+	greet("Tony", 100)
+}
+
+func greet(name string, edad uint8) {
+	fmt.Printf("Hola %s tienes %d años de edad \n", name, edad)
+}
